477ac5917f3e9c5ae2311e7b7e1b2fc443aaabe7
OrcClaSH <orcaclash@gmail.com>
2024-02-12 09:35:44 +0900

    feat: implement AppGateway

--- src/app/app.gateway.ts
+++ src/app/app.gateway.ts
@@ -1,9 +1,37 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+import { MessageDto } from 'src/message/dto';
+import { MessageService } from 'src/message/message.service';
 
 @WebSocketGateway()
-export class AppGateway {
-  @SubscribeMessage('message') // Подписка по событиям на websockets
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+export class AppGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  constructor(private readonly messageService: MessageService) {}
+
+  @WebSocketServer() server: Server;
+  @SubscribeMessage('sendMessage') // Подписка по событиям на websockets
+  async handleSendMessage(client: Socket, payload: MessageDto): Promise<void> {
+    await this.messageService.createMessage(payload);
+    this.server.emit('recMessage', payload);
+  }
+
+  afterInit(server: any) {
+    console.log({ server });
+  }
+
+  handleConnection(client: Socket) {
+    console.log(`Connected client: ${client.id}`);
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log(`Disconnected client: ${client.id}`);
   }
 }
