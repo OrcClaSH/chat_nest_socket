b041a00a5328018d24867b0a898ed63098f0773c
OrcClaSH <orcaclash@gmail.com>
2024-02-12 09:37:46 +0900

    feat: add chat endpoints

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,32 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Response } from 'express';
+import { MessageService } from './message/message.service';
+import { ApiOkResponse } from '@nestjs/swagger';
+import { MessageDto } from './message/dto';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly messageService: MessageService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('chat')
+  @Render('index')
+  Chat(): string {
+    return;
+  }
+
+  @Get('api/chat')
+  @ApiOkResponse({ type: [MessageDto] })
+  async getMessages(@Res() res: Response) {
+    const messages = await this.messageService.getMessages();
+    return res.json(messages);
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppGateway } from './app/app.gateway';
+import { DbModule } from './db/db.module';
+import { MessageModule } from './message/message.module';
+import { MessageService } from './message/message.service';
+import { DbService } from './db/db.service';
 
 @Module({
-  imports: [],
+  imports: [DbModule, MessageModule],
   controllers: [AppController],
-  providers: [AppService, AppGateway],
+  providers: [AppService, AppGateway, MessageService, DbService],
 })
 export class AppModule {}
